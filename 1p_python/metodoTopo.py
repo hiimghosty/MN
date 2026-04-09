e0a911ff0a950e16c8d79421add0c77c5775286e
hiimghosty00 <mauricioeliasbenitez@gmail.com>
2026-04-09 00:01:12 -0300

    topo

--- 1p_python/metodoTopo.py
+++ 1p_python/metodoTopo.py
@@ -1,3 +1,4 @@
+import jax.numpy as jnp
 import numpy as np
 
 #   Declaracion de DATOS
@@ -34,4 +35,26 @@ print("La densidad correspondiente a una temperatura de 2,4 es: ", np.around(q(2
 #   ITEM b)
 print("El error para una densidad de 999.952 es: ", 999.952 - q(2.4))
 
-# ITEM c)
+
+#   ITEM c)
+p0 = 4.0
+n = 50
+tol = 1e-2
+
+
+f = np.polyder(q)
+df = np.polyder(f)
+p0 = 4.0
+tol = 1e-2
+n = 50
+################RUTINA006########################## #NEWTON RHAPSON
+for i in range(n):
+    p = p0 - f(p0) / df(p0)
+    err = jnp.abs(p - p0)
+    relerr = jnp.abs(err / p)
+    if tol > err or tol > relerr or tol > jnp.abs(f(p)):
+        break
+    p0 = p
+
+
+print(f"c) Para densidad maxima la temperatura es: {p:0.6f}")
